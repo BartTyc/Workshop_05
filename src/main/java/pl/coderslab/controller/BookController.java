466e3417ad6c902233936cfc04f30ca94ee18d06
BartTyc <bartosz.tyczko@gmail.com>
2018-04-20 15:14:31 +0200

    zadanie6

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,12 +1,24 @@
 package pl.coderslab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.Service.MemoryBookService;
 import pl.coderslab.model.Book;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private MemoryBookService memoryBookService;
+
+    @Autowired
+    public BookController(MemoryBookService memoryBookService) {
+        this.memoryBookService = memoryBookService;
+    }
+
     @RequestMapping("/hello")
     public String hello(){
         return "{hello: World}";
@@ -16,5 +28,10 @@ public class BookController {
     public Book helloBook(){
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
+    }
+
+    @RequestMapping("booksList")
+    public List<Book> bookList () {
+        return memoryBookService.getList();
     }
 }
